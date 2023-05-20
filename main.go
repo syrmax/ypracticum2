f46bb62a463046b71f3aa7849e7f5f43a4ca1c99
syrmax <syrmax@gmail.com>
2023-05-20 19:58:54 +0700

    Наполнение файлов

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"yourpackage" // ваш пакет
+)
+
+func main() {
+	if sum := yourpackage.Add(1, 2); sum != 3 {
+		panic(fmt.Sprintf("sum expected to be 3; got %d", sum))
+	}
+
+	fmt.Println("Well done!")
 }
